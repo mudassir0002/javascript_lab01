7261e48a2eddc97f1a1c88c7e5fd86c7974297a0
mudassir0002 <mudassirsiddiqui03@gmail.com>
2024-03-26 21:58:27 +0530

    sol4

---
+++ main.js
@@ -0,0 +1,8 @@
+const {add,subtract,multiply,divide}=require('./math');
+let a=45;
+let b=5;
+
+console.log("Addition: ",add(a,b));
+console.log("Subtraction: ",subtract(a,b));
+console.log("Multiplication: ",multiply(a,b));
+console.log("Divide: ",divide(a,b));
